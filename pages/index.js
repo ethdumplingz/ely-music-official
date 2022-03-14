e6efbfe2e7eef1f57039678e6ea418dd0f505097
ethdumplingz <94034316+ethdumplingz@users.noreply.github.com>
2022-03-14 13:37:12 -0400

    Removed boilerplate content from the beginning

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Link from 'next/link'
 const Index = () => (
   <div>
-    Hello World.{' '}
-    <Link href="/about">
-      <a>About</a>
-    </Link>
+    Site coming.
+    {/*Hello World.{' '}*/}
+    {/*<Link href="/about">*/}
+    {/*  <a>About</a>*/}
+    {/*</Link>*/}
   </div>
 )
 export default Index;
