7a76aa9512bf6126c1a97bfca658ef38d90ba3cf
ethdumplingz <94034316+ethdumplingz@users.noreply.github.com>
2022-03-14 18:10:24 -0400

    Main splash image was not responsive

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,86 @@
 import {AppBar, Container, Toolbar, Typography, Grid, IconButton} from "@mui/material";
 import TwitterIcon from "@mui/icons-material/Twitter";
-const Index = () => (
-  <>
-    <Container
-      maxWidth={"lg"}
-    >
-      <AppBar
-        position={"static"}
-      >
-        <Toolbar>
-          <Grid
-            container
-            justifyContent={"space-between"}
-            alignItems={"center"}
-            flexWrap={"nowrap"}
-          >
-            <Grid item>
-              <Typography
-                sx={{
-                  fontSize: "1.4rem"
-                }}
-              >Ely</Typography>
-            </Grid>
-            <Grid
-              item
-            >
-              <a target={"_blank"} href={"https://twitter.com/ratkingnft/"}>
-                <IconButton
-                  sx={{
-                    color: "#FFFFFF"
-                  }}
-                >
-                  <TwitterIcon />
-                </IconButton>
-              </a>
-            </Grid>
-          </Grid>
-        </Toolbar>
-      </AppBar>
-      <Grid
-        container
-        flexDirection={"column"}
-        alignItems={"center"}
-      >
-        <Grid
-          item
-          sx={{
-            pt: 5,
-            pb: 5,
-            textAlign: "center"
-          }}
-        >
-          <img
-            alt={"Ely Official Music"} src={require("../images/ely_hires.jpeg")}
-            style={{
-              width: {
-                xs: "100%",
-                md: "80%"
-            },
-              maxWidth: "1024px",
-              height: "auto"
-            }}
-          />
-        </Grid>
-        <Grid item>
-          <Typography
-            sx={{
-              fontWeight: 100,
-              fontSize: "1.5rem",
-              textAlign: "center",
-            }}
-          >
-            Site coming soon.
-          </Typography>
-        </Grid>
-      </Grid>
-    </Container>
-  </>
+import {useTheme} from "@mui/material/styles"
+
+const Index = () => {
+  const theme = useTheme();
   
-)
+  return(
+    <>
+      <Container
+        maxWidth={"lg"}
+      >
+        <AppBar
+          position={"static"}
+        >
+          <Toolbar>
+            <Grid
+              container
+              justifyContent={"space-between"}
+              alignItems={"center"}
+              flexWrap={"nowrap"}
+            >
+              <Grid item>
+                <Typography
+                  sx={{
+                    fontSize: "1.4rem"
+                  }}
+                >Ely</Typography>
+              </Grid>
+              <Grid
+                item
+              >
+                <a target={"_blank"} href={"https://twitter.com/ratkingnft/"}>
+                  <IconButton
+                    sx={{
+                      color: "#FFFFFF"
+                    }}
+                  >
+                    <TwitterIcon />
+                  </IconButton>
+                </a>
+              </Grid>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+        <Grid
+          container
+          flexDirection={"column"}
+          alignItems={"center"}
+        >
+          <Grid
+            item
+            sx={{
+              pt: 5,
+              pb: 5,
+              textAlign: "center"
+            }}
+          >
+            <img
+              alt={"Ely Official Music"} src={require("../images/ely_hires.jpeg")}
+              style={{
+                width: theme.breakpoints.down('md') ? '100%' : '80%',
+                maxWidth: "1024px",
+                height: "auto"
+              }}
+            />
+          </Grid>
+          <Grid item>
+            <Typography
+              sx={{
+                fontWeight: 100,
+                fontSize: "1.5rem",
+                textAlign: "center",
+              }}
+            >
+              Site coming soon.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  )
+  
+}
+
 export default Index;
